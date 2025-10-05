7bffd0c08ab4d5ebce165375c384c1330cdf0d20
IbrahimRaja7 <ibrahimmomin16@gmail.com>
2025-10-05 19:38:35 +0500

    Update whirlpool-titans.py

--- whirlpool-titans.py
+++ whirlpool-titans.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import plotly.graph_objects as go
+
 import plotly.express as px
 from datetime import datetime, timedelta
 import requests
